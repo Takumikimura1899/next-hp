d1e0d4335d0b333e176c75fea37924a17d5e49b3
Takumikimura141899 <kimura141899@gmail.com>
2021-09-09 20:07:40 +0900

    途中

--- posts/[id].tsx
+++ posts/[id].tsx
@@ -2,6 +2,42 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import { getAllPostIds, getPostData } from '../lib/posts';
 
+export default function Post({
+  post,
+}: {
+  post: { title: string; id: string; body: string };
+}) {
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Layout title={post.title}>
+      <p className='m-4'>{post.id}</p>
+      <p className='mb-8 text-xl font-bold'>{post.title}</p>
+      <p className='px-10'>{post.body}</p>
+      <Link href='/blog-page'>
+        <a className='flex cursor-pointer mt-12'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6 w-6 mr-3'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M11 19l-7-7 7-7m8 14l-7-7 7-7'
+            />
+          </svg>
+          <span>Back to blog-page</span>
+        </a>
+      </Link>
+    </Layout>
+  );
+}
+
 export async function getStaticPaths() {
   const paths = await getAllPostIds();
 
